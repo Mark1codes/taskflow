bb1f344aa7c1840a3691c36b485f35b6191ff56f
Mark <147322828+Mark1codes@users.noreply.github.com>
2025-06-20 00:42:52 +0800

    f

--- app/api/auth/callback/route.tsx
+++ app/api/auth/callback/route.tsx
@@ -1,6 +1,6 @@
 
      import { NextResponse } from 'next/server'
-     import supabase from '../../../utils/supabase'
+     import supabase from '../../../../utils/supabase'
 
      export const dynamic = 'force-dynamic'
 
