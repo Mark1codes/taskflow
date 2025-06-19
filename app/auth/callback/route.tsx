a276981a6efb658c512f04daf050f684ab395763
Mark <147322828+Mark1codes@users.noreply.github.com>
2025-06-20 00:35:19 +0800

    f

--- app/auth/callback/route.tsx
+++ app/auth/callback/route.tsx
@@ -1,13 +1,17 @@
-import { NextResponse } from 'next/server'
-import supabase from '../../../utils/supabase'
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const code = searchParams.get('code')
-  const state = searchParams.get('state')
-  if (code) {
-    await supabase.auth.exchangeCodeForSession(code)
-  }
-  
-  return NextResponse.redirect(new URL('/', request.url))
-}
+     import { NextResponse } from 'next/server'
+     import supabase from '../../../utils/supabase'
+
+     export const dynamic = 'force-dynamic'
+
+     export async function GET(request: Request) {
+       const { searchParams } = new URL(request.url)
+       const code = searchParams.get('code')
+       const state = searchParams.get('state')
+       if (code) {
+         await supabase.auth.exchangeCodeForSession(code)
+       }
+       
+       return NextResponse.redirect(new URL('/', request.url))
+     }
+     
